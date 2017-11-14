bf052b2692eaa2ce1e23337385ddaed9199515a8
Daniela Ruiz <daru015@gmail.com>
2017-11-14 16:48:19 +0100

    Square Component test example

--- __tests__/square-function.test.js
+++ __tests__/square-function.test.js
@@ -1,3 +1,30 @@
 /**
- * Created by daru on 14/11/2017.
+ * Created by Daniela Ruiz - daru015@gmail.com
+ * Date: 09/11/2017
+ * File: square-function.test.js
  */
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Square from '../src/components/Square.js';
+
+const i = 0;
+const props = { value: 'X', letterClass: "blue-letters", onClick : (x)=>{} };
+
+describe('Square', () => {
+
+    describe('Where the props.letterClass is blue-letters', () => {
+        it('should render blue-letters style', () => {
+
+            const component = renderer.create(
+                <Square value={props.value} onClick={() => props.onClick(i)} letterClass={props.letterClass} />
+            );
+
+            let tree = component.toJSON();
+            expect(tree).toMatchSnapshot();
+
+        });
+    });
+
+});
