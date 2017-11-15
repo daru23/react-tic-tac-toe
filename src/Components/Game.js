559a356403d23065464156ee14ac3922375fb909
Daniela Ruiz <daru015@gmail.com>
2017-11-15 14:10:34 +0100

    style class fixed

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -91,7 +91,7 @@ export default class Game extends React.Component {
         }
 
         return (
-            <div className="game main-container">
+            <div className="game">
                 <div className="game-board">
                     <Board
                         squares={current.squares}
