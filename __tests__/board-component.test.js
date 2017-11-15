e5c5fcb4d32cb4e340afa8ec3f0f35f9c7f2ad1e
Daniela Ruiz <daru015@gmail.com>
2017-11-15 16:25:35 +0100

    imports necessary for tests

--- __tests__/board-component.test.js
+++ __tests__/board-component.test.js
@@ -1,3 +1,16 @@
 /**
- * Created by daru on 15/11/2017.
+ * Created by Daniela Ruiz - daru015@gmail.com
+ * Date: 09/11/2017
+ * File: board-component.test.js
  */
+
+import React from 'react';
+import { expect, assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import Square from '../src/Components/Board.js';
+
+const EnzymeAdapter = require('enzyme-adapter-react-16');
+
+const Enzyme = require('enzyme');
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
