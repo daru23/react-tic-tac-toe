a4b4b44b0d599f6f7dc629bb6dd49b3803e4838c
Daniela Ruiz <daru015@gmail.com>
2017-11-15 13:34:06 +0100

    fixed capital letter in components folder

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './components/Game.js'
+import Game from './Components/Game.js'
 
 ReactDOM.render(
     <Game />,
