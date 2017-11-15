ad2f877e0caf4efafdc1d061056be6a61279eb60
Daniela Ruiz <daru015@gmail.com>
2017-11-15 16:10:17 +0100

    checking value

--- __tests__/square-function.test.js
+++ __tests__/square-function.test.js
@@ -5,24 +5,29 @@
  */
 
 import React from 'react';
-import renderer from 'react-test-renderer';
-
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
 import Square from '../src/Components/Square.js';
 
-const i = 0;
-const props = { value: 'X', letterClass: "blue-letters", onClick : (x)=>{} };
+const EnzymeAdapter = require('enzyme-adapter-react-16');
+
+const Enzyme = require('enzyme');
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 describe('Square', () => {
+
+    const i = 0;
+    const props = { value: 'X', letterClass: "blue-letters", onClick : (x)=>{} };
 
     describe('Where the props.letterClass is blue-letters', () => {
         it('should render blue-letters style', () => {
 
-            const component = renderer.create(
+            const square = mount(
                 <Square value={props.value} onClick={() => props.onClick(i)} letterClass={props.letterClass} />
             );
 
-            let tree = component.toJSON();
-            expect(tree).toMatchSnapshot();
+            expect(square.props().value).to.equal('X');
 
         });
     });
