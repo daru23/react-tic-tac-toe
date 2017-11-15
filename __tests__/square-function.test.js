0ae3805063da9de3912ddbb09588fb4f060b6b94
Daniela Ruiz <daru015@gmail.com>
2017-11-15 16:20:50 +0100

    more tests for Square Component

--- __tests__/square-function.test.js
+++ __tests__/square-function.test.js
@@ -5,7 +5,7 @@
  */
 
 import React from 'react';
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import { shallow, mount } from 'enzyme';
 import Square from '../src/Components/Square.js';
 
@@ -21,15 +21,33 @@ describe('Square', () => {
     const props = { value: 'X', letterClass: "blue-letters", onClick : (x)=>{} };
 
     describe('Where the props.letterClass is blue-letters', () => {
+
         it('should render blue-letters style', () => {
+            const square = mount(<Square value={props.value} onClick={() => props.onClick(i)} letterClass={props.letterClass} />);
 
-            const square = mount(
-                <Square value={props.value} onClick={() => props.onClick(i)} letterClass={props.letterClass} />
-            );
-
-            expect(square.props().value).to.equal('X');
-
+            expect(square.props().letterClass).to.equal('blue-letters');
         });
+
+        it('should not render red-letters style', () => {
+            const square = mount(<Square value={props.value} onClick={() => props.onClick(i)} letterClass={props.letterClass} />);
+
+            assert.notEqual(square.props().letterClass, 'red-letters');
+        });
+
+        it('should have value X', () => {
+            const square = mount(<Square value={props.value} onClick={() => props.onClick(i)} letterClass={props.letterClass} />);
+
+            assert.equal(square.props().value, 'X');
+        });
+
+        it('should not have value O', () => {
+            const square = mount(<Square value={props.value} onClick={() => props.onClick(i)} letterClass={props.letterClass} />);
+
+            assert.notEqual(square.props().value, 'O');
+        });
+
     });
+
+
 
 });
